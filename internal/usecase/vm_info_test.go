b5d414f0692afdbcf3a8d9254ae6c4a37390238a
vterdunov <mail2slick@gmail.com>
2019-06-11 20:05:52 +0400

    Use yable tests for tests

--- internal/usecase/vm_info_test.go
+++ internal/usecase/vm_info_test.go
@@ -3,15 +3,71 @@ package usecase_test
 import (
 	"testing"
 
-	"github.com/vterdunov/janna/internal/usecase"
+	"github.com/stretchr/testify/assert"
+
+	usecase "github.com/vterdunov/janna/internal/usecase"
 )
 
 func TestUsecase_VMInfo(t *testing.T) {
-	// TODO: switch to table test
-	vmWareRepositoryMock := new(VMWareRepository)
-	vmWareRepositoryMock.On("VMInfo", "ddd").Return(usecase.VMInfoResponse{}, nil)
-	u := usecase.NewUsecase(nil, vmWareRepositoryMock)
-	_, _ = u.VMInfo("ddd")
+	tests := []struct {
+		name      string
+		arg       string
+		want      usecase.VMInfoResponse
+		wantError bool
+		prepare   func(*VMWareRepository)
+	}{
+		{
+			name:      "test",
+			arg:       "ddd",
+			want:      usecase.VMInfoResponse{},
+			wantError: false,
+			prepare: func(c *VMWareRepository) {
+				c.On("VMInfo", "ddd").Return(usecase.VMInfoResponse{}, nil).Once()
+			},
+		},
+		{
+			name:      "test2",
+			arg:       "dddd",
+			want:      usecase.VMInfoResponse{},
+			wantError: true,
+			prepare: func(mock *VMWareRepository) {
+				mock.On("VMInfo", "dddd").Return(usecase.VMInfoResponse{}, nil).Once()
+			},
+		},
+	}
 
-	vmWareRepositoryMock.AssertExpectations(t)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			mock := new(VMWareRepository)
+
+			if tt.prepare != nil {
+				tt.prepare(mock)
+			}
+
+			u := usecase.NewUsecase(nil, mock)
+
+			// run the tested function
+			got, err := u.VMInfo(tt.arg)
+
+			if tt.wantError {
+				assert.NotNil(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, tt.want, got) // notice the go convention: want is first argument, got is the second argument
+			}
+
+			// assert that the mocks were called correctly.
+			mock.AssertExpectations(t)
+		})
+	}
+
+	// TODO: switch to table test
+	// vmWareRepositoryMock := new(VMWareRepository)
+	// vmWareRepositoryMock.On("VMInfo", "ddd").Return(usecase.VMInfoResponse{}, nil)
+	// u := usecase.NewUsecase(nil, vmWareRepositoryMock)
+	// _, _ = u.VMInfo("ddd")
+
+	// vmWareRepositoryMock.AssertExpectations(t)
 }
