802e1d0c3be210cee718cec8fa766007161e6150
vterdunov <mail2slick@gmail.com>
2019-07-15 12:46:42 +0400

    regenerate mocks

--- internal/virtualmachine/mocks/vm_repository.go
+++ internal/virtualmachine/mocks/vm_repository.go
@@ -32,20 +32,20 @@ func (_m *VMRepository) IsVMExist(_a0 context.Context, _a1 string, _a2 string) (
 	return r0, r1
 }
 
-// VMDeploy provides a mock function with given fields: params
-func (_m *VMRepository) VMDeploy(params virtualmachine.VMDeployRequest) (virtualmachine.VMDeployResponse, error) {
-	ret := _m.Called(params)
+// VMDeploy provides a mock function with given fields: _a0, _a1
+func (_m *VMRepository) VMDeploy(_a0 context.Context, _a1 virtualmachine.VMDeployRequest) (virtualmachine.VMDeployResponse, error) {
+	ret := _m.Called(_a0, _a1)
 
 	var r0 virtualmachine.VMDeployResponse
-	if rf, ok := ret.Get(0).(func(virtualmachine.VMDeployRequest) virtualmachine.VMDeployResponse); ok {
-		r0 = rf(params)
+	if rf, ok := ret.Get(0).(func(context.Context, virtualmachine.VMDeployRequest) virtualmachine.VMDeployResponse); ok {
+		r0 = rf(_a0, _a1)
 	} else {
 		r0 = ret.Get(0).(virtualmachine.VMDeployResponse)
 	}
 
 	var r1 error
-	if rf, ok := ret.Get(1).(func(virtualmachine.VMDeployRequest) error); ok {
-		r1 = rf(params)
+	if rf, ok := ret.Get(1).(func(context.Context, virtualmachine.VMDeployRequest) error); ok {
+		r1 = rf(_a0, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
